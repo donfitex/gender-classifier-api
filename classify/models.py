1fa272b8b1ea71ce4c4eef8e5fe28a23dde736cd
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-15 19:33:00 +0100

    Implement UUID v7 using uuid6 package

--- classify/models.py
+++ classify/models.py
@@ -1,11 +1,10 @@
-from uuid import uuid7
-import uuid
+from uuid6 import uuid7
 from django.db import models
 
 # Create your models here.
 
 class Profile(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
     name = models.CharField(max_length=150, unique=True)
     gender = models.CharField(max_length=20)
     gender_probability = models.FloatField(max_length=10)
