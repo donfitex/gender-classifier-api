0f507d7f50a3979d8dab61074aa3592c4ff96d3e
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-15 21:38:33 +0100

    Add route for getting single profile, getting all profiles, and deleting profile

--- classify/urls.py
+++ classify/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import classify_name, create_profile
+from .views import classify_name, create_profile, list_profiles, get_profile, delete_profile
+
 
 urlpatterns = [
     path('classify/', classify_name),
     path('profiles/', create_profile),
+    path('profiles/', list_profiles),
+    path('profiles/<uuid:id>/', get_profile),
+    path('profiles/<uuid:id>/', delete_profile),
 ]
