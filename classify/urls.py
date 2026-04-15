fc28768f51fb8b554919b4e50f6469361e860662
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-15 22:05:57 +0100

    Update profile route

--- classify/urls.py
+++ classify/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import classify_name, create_profile, list_profiles, get_profile, delete_profile
+from .views import classify_name, profiles, get_profile, delete_profile
 
 
 urlpatterns = [
     path('classify/', classify_name),
-    path('profiles/', create_profile),
-    path('profiles/', list_profiles),
+    path('profiles/', profiles),
     path('profiles/<uuid:id>/', get_profile),
     path('profiles/<uuid:id>/', delete_profile),
 ]
