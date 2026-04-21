dae6657cbda90d824a247c40d710f37b9c880419
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-21 18:43:58 +0100

    feat(setting): configure settings and install dependency to allow filtering and pagination

--- gender_classify_api/settings/base.py
+++ gender_classify_api/settings/base.py
@@ -29,6 +29,13 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
     "DEFAULT_PERMISSION_CLASSES": [],
+    "DEFAULT_FILTER_BACKENDS": [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 
