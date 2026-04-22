551139073147d3e08a879e46f679793cb21bc32e
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-22 12:31:56 +0100

    feat(api): add profile search endpoint

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import profiles, profile_detail
+from .views import profiles, profile_detail, search_profiles
 
 urlpatterns = [
     path('api/profiles', profiles),
     path('api/profiles/', profiles),
+    path('api/search', search_profiles),
     path('api/profiles/<uuid:id>', profile_detail),
     path('api/profiles/<uuid:id>/', profile_detail),
 ]
