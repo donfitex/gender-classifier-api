184002e156f1158dbe0637f42388f3e347dcf0bf
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-03 16:01:25 +0100

    feat(oauth): add functions for generating code challenge and building GitHub OAuth URL

---
+++ users/services/oauth_service.py
@@ -0,0 +1,21 @@
+import os
+import base64
+import hashlib
+
+def generate_code_challenge(verifier):
+    return base64.urlsafe_b64encode(
+        hashlib.sha256(verifier.encode()).digest()
+    ).decode().rstrip("=")
+
+
+def build_github_url(state, code_challenge):
+    client_id = os.getenv("GITHUB_CLIENT_ID")
+
+    return (
+        "https://github.com/login/oauth/authorize"
+        f"?client_id={client_id}"
+        f"&scope=user"
+        f"&state={state}"
+        f"&code_challenge={code_challenge}"
+        f"&code_challenge_method=S256"
+    )
