c26660ba8cd331478137029acb5d4622c408952e
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-03 16:33:42 +0100

    feat(urls): add GitHub login path to URL patterns

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import github_callback, github_callback_web
+from .views import github_callback, github_callback_web, github_login
 
 urlpatterns = [
+    path('auth/github', github_login),
     path('auth/github/callback', github_callback),
     path('auth/github/callback/web', github_callback_web),
 ]
