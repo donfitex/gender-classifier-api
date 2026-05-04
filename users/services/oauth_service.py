59ad068dc5fe838613e666f4582a13ba4c0fedb2
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-04 12:07:08 +0100

    feat(oauth): enhance GitHub OAuth flow with code verifier and challenge generation

--- users/services/oauth_service.py
+++ users/services/oauth_service.py
@@ -1,6 +1,14 @@
 import os
 import base64
 import hashlib
+import secrets
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_code_verifier():
+    return secrets.token_urlsafe(64)
+
 
 def generate_code_challenge(verifier):
     return base64.urlsafe_b64encode(
@@ -10,10 +18,12 @@ def generate_code_challenge(verifier):
 
 def build_github_url(state, code_challenge):
     client_id = os.getenv("GITHUB_CLIENT_ID")
-
+    redirect_uri = os.getenv('GITHUB_REDIRECT_URI')
+    
     return (
         "https://github.com/login/oauth/authorize"
         f"?client_id={client_id}"
+        f"&redirect_uri={redirect_uri}"
         f"&scope=user"
         f"&state={state}"
         f"&code_challenge={code_challenge}"
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 import os
 import secrets
 import requests
+from dotenv import load_dotenv
 from django.utils import timezone
 from .models import User
 from .utils import web_generate_tokens
@@ -17,27 +18,41 @@ from .services.user_service import create_or_update_user
 from .services.token_services import generate_tokens
 
 from django.http import JsonResponse
-from .services.oauth_service import build_github_url
+from .services.oauth_service import build_github_url, generate_code_verifier, generate_code_challenge
 
-STATE_STORE = set()
+load_dotenv()
+
+STATE_STORE = {} # In-memory store for valid states (for demo purposes only)
 
 #Github OAuth views
 def github_login(request):
-    state = secrets.token_urlsafe(16)
-    code_challenge = request.GET.get("code_challenge")  # from CLI
-    STATE_STORE.add(state)
-    url = build_github_url(state, code_challenge)
+    
+    is_cli = request.GET.get("cli") == "true"
+    if is_cli:
+        
+        # Generate code verifier and challenge
+        code_verifier = generate_code_verifier()
+        code_challenge = generate_code_challenge(code_verifier)
 
-    return JsonResponse({
-        "auth_url": url,
-        "state": state
-    })
+        # Generate random state for CSRF protection
+        state = secrets.token_urlsafe(16) 
+        request.session["oauth_state"] = state
+        request.session["code_verifier"] = code_verifier
+
+        STATE_STORE[state] = code_verifier
+
+        url = build_github_url(state, code_challenge)
+
+        return JsonResponse({
+            "auth_url": url,
+            "state": state
+        })
 
 # API callback for GitHub OAuth (for mobile/third-party use)
-@api_view(['POST'])
-def github_callback(request):
-    code = request.data.get("code")
-    code_verifier = request.data.get("code_verifier")
+@api_view(['GET'])
+def github_callback_web(request):
+    code = request.GET.get("code")
+    code_verifier = request.GET.get("code_verifier")
 
     if not code or not code_verifier:
         return Response(
@@ -82,11 +97,12 @@ def github_callback(request):
 
 # Web callback for GitHub OAuth (for frontend use)
 @api_view(['GET'])
-def github_callback_web(request):
+def github_callback(request):
     code = request.GET.get("code")
     state = request.GET.get("state")
-    code_verifier = request.GET.get("code_challenge")
-    if state not in STATE_STORE:
+    code_verifier = STATE_STORE.get(state)
+
+    if not code_verifier:
         return JsonResponse(
             {"status": "error", "message": "Invalid state"},
             status=400
@@ -149,5 +165,48 @@ def github_callback_web(request):
             "username": user.username,
             "avatar_url": user.avatar_url,
             "role": user.role
+        }
+    })
+
+
+@api_view(["POST"])
+def exchange_token(request):
+    code = request.data.get("code")
+    code_verifier = request.data.get("code_verifier")
+
+    token_res = requests.post(
+        "https://github.com/login/oauth/access_token",
+        headers={"Accept": "application/json"},
+        data={
+            "client_id": os.getenv("GITHUB_CLIENT_ID"),
+            "client_secret": os.getenv("GITHUB_CLIENT_SECRET"),
+            "code": code,
+            "code_verifier": code_verifier,
+        }
+    ).json()
+
+    access_token = token_res.get("access_token")
+
+    user_res = requests.get(
+        "https://api.github.com/user",
+        headers={"Authorization": f"Bearer {access_token}"}
+    ).json()
+
+    user, _ = User.objects.get_or_create(
+        github_id=user_res["id"],
+        defaults={
+            "username": user_res["login"],
+            "avatar_url": user_res["avatar_url"]
+        }
+    )
+
+    access, refresh = generate_tokens(user)
+
+    return Response({
+        "status": "success",
+        "access_token": access,
+        "refresh_token": refresh,
+        "user": {
+            "username": user.username
         }
     })
