c566b1691454dff6bc6000f605e6960e4c3facfd
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-04 12:08:13 +0100

    feat(urls): add exchange token path to URL patterns

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import github_callback, github_callback_web, github_login
+from .views import github_callback, github_callback_web, github_login, exchange_token
 
 urlpatterns = [
     path('auth/github', github_login),
     path('auth/github/callback', github_callback),
     path('auth/github/callback/web', github_callback_web),
+    path("auth/exchange", exchange_token),
 ]
