d6021e7e6d96d08450a5f5c2002e58b07008383e
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-06 10:22:40 +0100

    feat(token): refactor token generation to use time-based expiration and return expiration times

--- users/services/token_services.py
+++ users/services/token_services.py
@@ -1,5 +1,6 @@
 from uuid6 import uuid7
 from datetime import datetime, timedelta, timezone
+import time
 
 TOKENS = {}  # In-memory store for refresh tokens; in production, use a database or cache
 
@@ -8,11 +9,21 @@ def generate_tokens(user):
     access_token = str(uuid7())
     refresh_token = str(uuid7())
 
+    # TOKENS[refresh_token] = {
+    #     "user_id": str(user.id),
+    #     "access_token": access_token,
+    #     "access_expires": datetime.now(timezone.utc) + timedelta(minutes=3),
+    #     "refresh_expires": datetime.now(timezone.utc) + timedelta(minutes=5),
+    # }
+
+    access_expires = time.time() + 180   # 3 minutes
+    refresh_expires = time.time() + 300  # 5 minutes
+
     TOKENS[refresh_token] = {
         "user_id": str(user.id),
         "access_token": access_token,
-        "access_expires": datetime.now(timezone.utc) + timedelta(minutes=3),
-        "refresh_expires": datetime.now(timezone.utc) + timedelta(minutes=5),
+        "access_expires": access_expires,
+        "refresh_expires": refresh_expires
     }
 
-    return access_token, refresh_token
+    return access_token, refresh_token, access_expires, refresh_expires
