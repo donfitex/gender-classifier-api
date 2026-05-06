32a8c9fa7a9cf5ac70d760b29d3021621b128883
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-06 10:24:02 +0100

    feat(urls): add refresh token path to URL patterns

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import github_login, github_callback_web, exchange_token
+from .views import github_login, github_callback_web, exchange_token, refresh_token
 
 urlpatterns = [
     path('auth/github', github_login),
     path('auth/github/callback/web', github_callback_web),
     path('auth/exchange', exchange_token),
+    path("auth/refresh", refresh_token),
 ]
